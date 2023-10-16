ef95b14b3e0dc12aba56675fbfffff41b58bd376
Mawuli Agropah <wizzyagrops2016@gmail.com>
2023-10-16 05:28:12 +0000

    AirBnB project

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This is a Module that creates the class BaseModel"""
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel():
+    """This defines all common attributes or methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "updated_at" or key == "created_at":
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """this prints an instance of the object specified"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """this updates the `updated_at` attribute with current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__
+        of the instance"""
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This module create a class called FileStorage"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    '''serializes instances to a JSON file and deserializes
+       JSON file to instances'''
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''this module returns the dict __objects'''
+        return self.__objects
+
+    def new(self, obj):
+        '''sets in `__objects` the `obj` with key `<obj class name>.id`'''
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        '''serializes `__objects` to the JSON file'''
+        with open(self.__file_path, "w", encoding="utf-8") as json_file:
+            dic = {k : v.to_dict() for k, v in self.__objects.items()}
+            json.dump(dic, json_file)
+
+    def reload(self):
+        '''deserializes the JSON file to `__objects`'''
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as json_file:
+                obj_dict = json.load(json_file)
+                for ob in obj_dict.values():
+                    class_name = ob["__class__"]
+                    del ob["__class__"]
+                    self.new(eval(class_name)(**ob))
+        except FileNotFoundError:
+            pass
