36f22f37915fb2f3400e288d2a65ed366283c878
Mawuli Agropah <wizzyagrops2016@gmail.com>
2023-10-16 05:59:11 +0000

    updated

---
+++ pycode_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# This is a simple Python script that prints a greeting message.
+
+
+def greet(name):
+    """Print a greeting message."""
+    print(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    user_name = "Alice"
+    greet(user_name)
